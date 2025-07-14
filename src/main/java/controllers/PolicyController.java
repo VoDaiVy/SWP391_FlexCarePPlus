63459e76eed4bc985b9e4efdaf2a53fdbdabb174
thanh25082004 <quangthanh0825@gmail.com>
2025-07-14 12:54:39 +0700

    Images, booking, booking to pay

--- src/main/java/controllers/PolicyController.java
+++ src/main/java/controllers/PolicyController.java
@@ -1,19 +1,17 @@
 package controllers;
 
-import daos.PolicyDAO;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import models.Policy;
 
 public class PolicyController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String actor = (String) request.getSession().getAttribute("actor");
+        String actor = (String)request.getSession().getAttribute("actor");
         switch (actor) {
             case "admin":
                 adminGet(request, response);
@@ -33,7 +31,7 @@ public class PolicyController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String actor = (String) request.getSession().getAttribute("actor");
+        String actor = (String)request.getSession().getAttribute("actor");
         switch (actor) {
             case "admin":
                 adminPost(request, response);
@@ -53,7 +51,7 @@ public class PolicyController extends HttpServlet {
     @Override
     protected void doPut(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String actor = (String) request.getSession().getAttribute("actor");
+        String actor = (String)request.getSession().getAttribute("actor");
         switch (actor) {
             case "admin":
                 adminPut(request, response);
@@ -73,7 +71,7 @@ public class PolicyController extends HttpServlet {
     @Override
     protected void doDelete(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String actor = (String) request.getSession().getAttribute("actor");
+        String actor = (String)request.getSession().getAttribute("actor");
         switch (actor) {
             case "admin":
                 adminDelete(request, response);
@@ -93,53 +91,12 @@ public class PolicyController extends HttpServlet {
     // Admin role methods
     private void adminGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String action = request.getParameter("action");
-        switch (action) {
-            case "getPolicies" -> {
-                request.setAttribute("policies", PolicyDAO.getAll());
-                request.getRequestDispatcher("adminPages/policies.jsp").forward(request, response);
-            }
-            case "getPolicyDetail" -> {
-                String idString = request.getParameter("id");
-                if (idString != null) {
-                    int policyID = Integer.parseInt(request.getParameter("id"));
-                    request.setAttribute("policy", PolicyDAO.getById(policyID));
-                }
-                request.getRequestDispatcher("adminPages/policyDetail.jsp").forward(request, response);
-            }
-        }
+        // To be implemented
     }
 
     private void adminPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String action = request.getParameter("action");
-        switch (action) {
-            case "updatePolicy" -> {
-                int id = Integer.parseInt(request.getParameter("id"));
-                String title = request.getParameter("title");
-                String description = request.getParameter("description");
-                Policy policy = PolicyDAO.getById(id);
-                policy.setTitle(title);
-                policy.setDescription(description);
-                PolicyDAO.update(policy);
-                response.sendRedirect("admin?action=getPolicyDetail&id=" + id);
-            }
-            case "createPolicy" -> {
-                String title = request.getParameter("title");
-                String description = request.getParameter("description");
-                Policy policy = new Policy();
-                policy.setTitle(title);
-                policy.setDescription(description);
-                policy.setStatus(true);
-                PolicyDAO.create(policy);
-                response.sendRedirect("admin?action=getPolicies");
-            }
-            case "deletePolicy" -> {
-                int id = Integer.parseInt(request.getParameter("id"));
-                PolicyDAO.hardDelete(id);
-                response.sendRedirect("admin?action=getPolicies");
-            }
-        }
+        // To be implemented
     }
 
     private void adminPut(HttpServletRequest request, HttpServletResponse response)
